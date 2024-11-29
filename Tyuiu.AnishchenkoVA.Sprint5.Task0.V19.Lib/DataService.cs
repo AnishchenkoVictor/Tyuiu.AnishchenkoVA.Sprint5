e4e57b6657bd6f97ecde81e266ba3be7935f9916
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-11-29 19:00:27 +0500

    Task0

--- Tyuiu.AnishchenkoVA.Sprint5.Task0.V19.Lib/DataService.cs
+++ Tyuiu.AnishchenkoVA.Sprint5.Task0.V19.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.AnishchenkoVA.Sprint5.Task0.V19.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.GetTempFileName();
-            double s = (2 * Math.Pow(x, 2) - 1) / (Math.Pow(Math.Pow(x, 2) - 2, 1 / 2));
+            double s = ((2 * Math.Pow(x, 2) - 1) / (Math.Pow(Math.Pow(x, 2) - 2, 0.5)));
             double sr = Math.Round(s, 3);
             File.WriteAllText(path, Convert.ToString(sr));
             return path;
