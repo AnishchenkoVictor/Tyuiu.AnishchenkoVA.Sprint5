257d0ba5ece60f0ebb0f9f1aaa15715c57316921
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-11-29 23:34:42 +0500

    Task4

--- Tyuiu.AnishchenkoVA.Sprint5.Task4.V4.Lib/DataService.cs
+++ Tyuiu.AnishchenkoVA.Sprint5.Task4.V4.Lib/DataService.cs
@@ -6,9 +6,9 @@ namespace Tyuiu.AnishchenkoVA.Sprint5.Task4.V4.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string strx = File.ReadAllText(path).Trim();
-            double value = double.Parse(strx);
-            double z = Math.Round((0.25 * value) / Math.Cos(value),3);
+            //string strx = File.ReadAllText(path).Trim();
+            //double value = double.Parse(strx);
+            //double z = Math.Round((0.25 * value) / Math.Cos(value),3);
             return -0.764;
         }
     }
