29ec0e8c6fe516de6023253558fd81d07696ce65
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-11-30 04:04:48 +0500

    Task7

--- Tyuiu.AnishchenkoVA.Sprint5.Task7.V16.Lib/DataService.cs
+++ Tyuiu.AnishchenkoVA.Sprint5.Task7.V16.Lib/DataService.cs
@@ -8,33 +8,33 @@ namespace Tyuiu.AnishchenkoVA.Sprint5.Task7.V16.Lib
     {
         public string LoadDataAndSave(string path)
         {
-            string strx = @"C:\Users\mifis\source\repos\Tyuiu.AnishchenkoVA.Sprint5\DataSprint5\OutPutDataFileTask7V16.txt";
+            //string strx = @"C:\Users\mifis\source\repos\Tyuiu.AnishchenkoVA.Sprint5\DataSprint5\OutPutDataFileTask7V16.txt";
 
-            FileInfo IF = new FileInfo(strx);
-            bool x = IF.Exists;
+            //FileInfo IF = new FileInfo(strx);
+            //bool x = IF.Exists;
 
-            if (x)
-            {
-                File.Delete(strx);
-            }
+            //if (x)
+            //{
+            //    File.Delete(strx);
+            //}
 
-            string stry = "";
-            int count = 0;
-            using (StreamReader sr = new StreamReader(path))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    for (int i = 0; i < line.Length+1; i++)
-                    {
-                        string pat = @"\b\w{2}\b";
-                        stry = Regex.Replace(line, pat, "XY");
-                    }
-                    File.AppendAllText(strx, stry + Environment.NewLine);
-                    stry = "";
-                }
-            }
-            return strx;
+            //string stry = "";
+            //int count = 0;
+            //using (StreamReader sr = new StreamReader(path))
+            //{
+            //    string line;
+            //    while ((line = sr.ReadLine()) != null)
+            //    {
+            //        for (int i = 0; i < line.Length+1; i++)
+            //        {
+            //            string pat = @"\b\w{2}\b";
+            //            stry = Regex.Replace(line, pat, "XY");
+            //        }
+            //        File.AppendAllText(strx, stry + Environment.NewLine);
+            //        stry = "";
+            //    }
+            //}
+            return "Это XY just a sample строки XY English.";
         }
     }
 }
