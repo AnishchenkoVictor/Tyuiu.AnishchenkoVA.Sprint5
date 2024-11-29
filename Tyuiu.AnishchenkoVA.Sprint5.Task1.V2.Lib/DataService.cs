71aeadacfa8f72fe98673caa854a8be839dfa4a9
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-11-29 19:57:25 +0500

    Task1

---
+++ Tyuiu.AnishchenkoVA.Sprint5.Task1.V2.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AnishchenkoVA.Sprint5.Task1.V2.Lib
+{
+    public class DataService : ISprint5Task1V2
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.GetTempFileName();
+
+            FileInfo FI = new FileInfo(path);
+            bool ex = FI.Exists;
+
+            if (ex)
+            {
+                File.Delete(path);
+            }
+
+            double s;
+            string stry;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                s = ((2 * x - 3) / (Math.Cos(x) - 2 * x)) + 5 * x - 6;
+                stry = Convert.ToString(s);
+
+                if (x != stopValue)
+                {
+                    File.AppendAllText(path, stry + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, stry);
+                }
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.AnishchenkoVA.Sprint5.Task1.V2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.AnishchenkoVA.Sprint5.Task1.V2.Lib;
+namespace Tyuiu.AnishchenkoVA.Sprint5.Task1.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Анищенко В. А. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Анищенко Виктор Александрович | ИИПБ-24-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3,                        *");
+            Console.WriteLine("* результат сохранить в текстовый файл OutPutFileTask0.txt                *");
+            Console.WriteLine("* и вывести на консоль. Округлить до трёх знаков после запятой.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int start = -5;
+            int stop = 5;
+
+            Console.WriteLine("Start = " + start);
+            Console.WriteLine("Stop = " + stop);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(start, stop);
+
+            Console.WriteLine(res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
